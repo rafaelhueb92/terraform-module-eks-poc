24ab4284bb3f9f6044e4b2601fba269395d84383
Rafael Hueb <rafaelhueb@192.168.1.4>
2026-07-24 16:07:18 -0300

    change the url to get the issuer

--- addons.tf
+++ addons.tf
@@ -23,9 +23,7 @@ provider "helm" {
 
 resource "aws_iam_openid_connect_provider" "eks" {
   count           = var.install_irsa ? 1 : 0
-  url             = replace(aws_eks_cluster.main.identity[0].oidc[0].issuer, "https://", "")
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0afd9e120"]
+  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
   tags            = local.common_tags
 }
 
