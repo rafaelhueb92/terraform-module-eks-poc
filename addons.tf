58a555fa7c4e37f84a155a7cf924bc3a656e3f2f
Rafael Hueb <rafaelhueb@192.168.1.4>
2026-07-24 16:10:05 -0300

    client_id_list set

--- addons.tf
+++ addons.tf
@@ -24,6 +24,7 @@ provider "helm" {
 resource "aws_iam_openid_connect_provider" "eks" {
   count           = var.install_irsa ? 1 : 0
   url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+  client_id_list  = ["sts.amazonaws.com"]
   tags            = local.common_tags
 }
 
